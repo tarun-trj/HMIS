309734976ccb46d540c706b366bfb80f376eb1d7
Sushant <sushant3660jee@gmail.com>
2025-04-11 08:25:41 +0530

    necessary

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -3,8 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 // TODO: Add routes for patient
-import { FetchPatientProfile, fetchConsultations, getAllDoctors,registerPatient } from '../controllers/patientController.js';
-import { FetchPatientProfile, fetchConsultations, getAllDoctors,sendFeedback } from '../controllers/patientController.js';
+import { FetchPatientProfile, fetchConsultations, getAllDoctors,registerPatient,sendFeedback  } from '../controllers/patientController.js';
 
 router.get('/profile/:patientId', FetchPatientProfile);
 router.get('/consultations/:patientId', fetchConsultations);
