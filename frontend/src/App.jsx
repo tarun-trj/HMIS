3296266df5e9032f27cfa60337f17759bcb83e9f
Adarsh Gupta <study.adarshgupta@gmail.com>
2025-04-15 19:41:56 +0530

    Fix Navigation Bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
