0d31a3887e544a62938d2f0f07810f1bccb04456
SimonShangpliang <simonrema123@gmail.com>
2025-04-18 17:22:27 +0530

    fixed bills.jsx bug

--- frontend/src/pages/patient/Bills.jsx
+++ frontend/src/pages/patient/Bills.jsx
@@ -6,7 +6,6 @@ import html2pdf from 'html2pdf.js';
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import "jspdf-autotable";
-import bill from "../../../../backend/models/bill";
 import { useAuth } from "../../context/AuthContext";
 
 // API base URL - adjust as needed
