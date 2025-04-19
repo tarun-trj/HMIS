e1eb81a35c77ae4cf4439009b97699f8de39f227
Adarsh Gupta <study.adarshgupta@gmail.com>
2025-04-19 22:34:00 +0530

    Minor ENV Variable Fix

--- backend/startWorker.js
+++ backend/startWorker.js
@@ -5,7 +5,7 @@ import './workers/notificationWorker.js';
 dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hmis')
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/hmis')
   .then(() => console.log('MongoDB connected for worker process'))
   .catch(err => console.error('MongoDB connection error:', err));
 
